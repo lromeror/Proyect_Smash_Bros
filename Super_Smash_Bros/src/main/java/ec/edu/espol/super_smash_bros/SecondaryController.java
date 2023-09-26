be33a7c5e63c22dd6ab31d5032f7a8c530f4dfc5
aszurita <nzuritag@fiec.espol.edu.ec>
2023-09-26 12:51:44 -0500

    Ya se reproduce videosE

--- Super_Smash_Bros/src/main/java/ec/edu/espol/super_smash_bros/SecondaryController.java
+++ Super_Smash_Bros/src/main/java/ec/edu/espol/super_smash_bros/SecondaryController.java
@@ -1,5 +1,6 @@
 package ec.edu.espol.super_smash_bros;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.animation.TranslateTransition;
@@ -7,6 +8,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Screen;
 import javafx.util.Duration;
 
@@ -51,6 +55,29 @@ public class SecondaryController implements Initializable{
 
         // Inicia la animación
         translateTransition.play();
+        String videoPath = "src/main/resources/Video_prueba/01_ Mario – Super Smash Bros. Ultimate.mp4"; // Reemplaza con la ruta a tu archivo de video
+        File videoFile = new File(videoPath);
+        String videoUrl = videoFile.toURI().toString();
+        
+        // Crear Media, MediaPlayer y MediaView
+        
+        Media media = new Media(videoUrl);
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        MediaView mediaView = new MediaView(mediaPlayer);
+                
+        // Posicionar el MediaView en la esquina superior izquierda
+        AnchorPane.setTopAnchor(mediaView, 0.0);
+        AnchorPane.setLeftAnchor(mediaView, 0.0);
+        double paneWidth = anchorPane.getWidth();
+        double paneHeight = anchorPane.getHeight();
+        mediaView.setFitWidth(paneWidth * 0.3); // Ejemplo: 30% del ancho del AnchorPane
+        mediaView.setFitHeight(paneHeight * 0.5); // Ejemplo: 50% del alto del AnchorPane
+
+        // Añadir MediaView al AnchorPane
+        anchorPane.getChildren().add(mediaView);
+
+        // Iniciar la reproducción del video
+        mediaPlayer.play();
     }
     
     private void adjustSizesAndPositions() {
