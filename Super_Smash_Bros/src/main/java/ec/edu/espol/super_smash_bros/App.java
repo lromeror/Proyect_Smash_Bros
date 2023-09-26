de814dd903a97384044706601d7aba7b41e75dc4
Nahin Cevallos <njcevall@espol.edu.ec>
2023-09-25 21:06:53 -0500

    Modificando la scene

--- Super_Smash_Bros/src/main/java/ec/edu/espol/super_smash_bros/App.java
+++ Super_Smash_Bros/src/main/java/ec/edu/espol/super_smash_bros/App.java
@@ -7,19 +7,25 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.stage.Screen;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
+    
 
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    stage.setFullScreen(true);
+    Parent root = FXMLLoader.load(getClass().getResource("secondary.fxml"));
+    scene = new Scene(root, Screen.getPrimary().getVisualBounds().getWidth(), Screen.getPrimary().getVisualBounds().getHeight());
+    stage.setScene(scene);
+    stage.show();
+
+    
     }
 
     static void setRoot(String fxml) throws IOException {
