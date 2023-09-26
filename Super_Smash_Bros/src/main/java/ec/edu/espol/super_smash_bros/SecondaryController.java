11510428a6616ce1d73aff7e9f80fcbfe219c896
Nahin Cevallos <njcevall@espol.edu.ec>
2023-09-26 10:40:00 -0500

    ajustes de tamaño

--- Super_Smash_Bros/src/main/java/ec/edu/espol/super_smash_bros/SecondaryController.java
+++ Super_Smash_Bros/src/main/java/ec/edu/espol/super_smash_bros/SecondaryController.java
@@ -6,6 +6,8 @@ import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Screen;
 import javafx.util.Duration;
 
 public class SecondaryController implements Initializable{
@@ -15,6 +17,8 @@ public class SecondaryController implements Initializable{
     private ImageView viñeta;
     @FXML
     private ImageView tortuga;
+    @FXML
+    private AnchorPane anchorPane;
     
     public void setPersonaje(Personaje p){
         this.personaje = p;
@@ -23,6 +27,16 @@ public class SecondaryController implements Initializable{
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        double screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
+        double screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
+
+        // Establece el tamaño del AnchorPane para que coincida con el tamaño de la pantalla
+        anchorPane.setPrefWidth(screenWidth);
+        anchorPane.setPrefHeight(screenHeight);
+        viñeta.fitWidthProperty().bind(anchorPane.widthProperty());
+        viñeta.fitHeightProperty().bind(anchorPane.heightProperty());
+        tortuga.fitWidthProperty().bind(anchorPane.widthProperty());
+        tortuga.fitHeightProperty().bind(anchorPane.heightProperty());
         TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(3), tortuga); // Duración de la animación en segundos
         translateTransition.setFromX(-tortuga.getFitWidth()); // Posición inicial (fuera del área visible a la izquierda)
         translateTransition.setToX(tortuga.getParent().getLayoutBounds().getWidth()); // Posición final (fuera del área visible a la derecha)
