f0d690645dc8a9cd20c61473df13a09b979e4936
aszurita <nzuritag@fiec.espol.edu.ec>
2023-09-27 17:48:24 -0500

    ok

--- Super_Smash_Bros/src/main/java/ec/edu/espol/super_smash_bros/SecondaryController.java
+++ Super_Smash_Bros/src/main/java/ec/edu/espol/super_smash_bros/SecondaryController.java
@@ -50,11 +50,8 @@ public class SecondaryController implements Initializable{
     @FXML
     private Button regresar;
     private MediaPlayer mediaPlayer;
-<<<<<<< Updated upstream
-    
+
     DataSingleton data = DataSingleton.getInstance();
-=======
->>>>>>> Stashed changes
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -169,19 +166,12 @@ public class SecondaryController implements Initializable{
 
     @FXML
     private void switchToPrimary(MouseEvent event) throws IOException {
-<<<<<<< Updated upstream
         
         data.cerrarSesion();
         if (mediaPlayer != null){
             mediaPlayer.stop();
         } 
-=======
-        SessionManager.getInstance().cerrarSesion();
-        // Detener la reproducción del video
-        if (mediaPlayer != null) {
-            mediaPlayer.stop();
-        }
->>>>>>> Stashed changes
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("primary.fxml"));
         Parent root = loader.load();
 //        Parent root = FXMLLoader.load(getClass().getResource("tablero.fxml"));
